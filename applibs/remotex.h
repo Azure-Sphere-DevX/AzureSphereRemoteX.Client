7cb15809c6d40b5c58bed01f1dd3567cfd3f7f57
Dave Glover <dglover@microsoft.com>
2021-12-08 10:26:15 +1100

    updated read write close and lseek overides

--- applibs/remotex.h
+++ applibs/remotex.h
@@ -7,7 +7,13 @@
 #include <stddef.h>
 
 int RemoteX_PlatformInformation(char *buffer, size_t buffer_length);
-int __wrap_close(int fd);
-off_t __wrap_lseek(int fd, off_t offset, int whence);
-ssize_t __wrap_read(int fd, void *buf, size_t count);
-ssize_t __wrap_write(int fd, const void *buf, size_t count);
+
+// int __wrap_close(int fd);
+// off_t __wrap_lseek(int fd, off_t offset, int whence);
+// ssize_t __wrap_read(int fd, void *buf, size_t count);
+// ssize_t __wrap_write(int fd, const void *buf, size_t count);
+
+int close(int fd);
+off_t lseek(int fd, off_t offset, int whence);
+ssize_t read(int fd, void *buf, size_t count);
+ssize_t write(int fd, const void *buf, size_t count);
